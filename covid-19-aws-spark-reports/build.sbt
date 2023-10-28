59bdf2c34207f04828acabeedd2d8e0978809055
ShaylinThadani <74334822+ShaylinThadani@users.noreply.github.com>
2023-10-28 18:38:15 -0400

    Delete covid-19-aws-spark-reports directory

--- covid-19-aws-spark-reports/build.sbt
+++
@@ -1,7 +0,0 @@
-name := "spark-covid"
-
-version := "1.0"
-
-scalaVersion := "2.11.12"
-
-libraryDependencies += "org.apache.spark" %% "spark-sql" % "2.4.5"
--- covid-19-aws-spark-reports/scripts/generateReports.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-git -C $REPO_COVID_PATH pull
-sbt package
-$SPARK_HOME/bin/spark-submit --class "ReportsGenerator" --master local[2]  target/scala-2.11/spark-covid_2.11-1.0.jar
